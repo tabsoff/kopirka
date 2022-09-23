f5c08d797375b702e0e932919b842b7ccc07caa8
tabsoff <tabstabs90@gmail.com>
2022-09-23 18:15:32 +0300

    Done somethings

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
-alert("Hello");
-
 function calculate() {
 
-	// Кол-во, А4, А3
-	const priceBW = [
-        [1, 29, 36],
+ // [ЧБ/цвет][кол-во][А4][А3]
+	const priceSheet = [
+		[[1, 29, 36],
         [6, 24, 29],
         [11, 21, 26],
         [21, 18, 23],
         [51, 17, 22],
         [101, 16, 21],
-        [501, 14, 19]
+        [501, 14, 19]],
+
+        [[1, 69, 84],
+        [6, 67, 75],
+        [11, 63, 68],
+        [21, 60, 65],
+        [51, 45, 50],
+        [101, 40, 45],
+        [200, 35, 40]]
 	];
 
 	var result;
 	var sheets = Number(document.getElementById("sheets").value);
 	var price;
 
+ 	var radio = document.getElementsByName('color');
+	for (var i = 0; i < radio.length; i++) {
+    	if (radio[i].checked) {
+      		color = i;
+      		break;
+    	}
+  	}
+
+  	radio = document.getElementsByName('size');
+	for (var i = 0; i < radio.length; i++) {
+    	if (radio[i].checked) {
+      		size = i + 1;
+      		break;
+    	}
+  	}
+
 	for (var i = 6; i >= 0; i--) {
-	   if (sheets >= priceBW[i][0]) {
-	   		price = priceBW[i][1];
+	   if (sheets >= priceSheet[color][i][0]) {
+	   		price = priceSheet[color][i][size];
 	   		break;
-	   }=
+	   }
 	}
 
 	result = sheets * price;
