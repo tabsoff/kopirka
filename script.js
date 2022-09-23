a5e9c15b4f8df0bdd6b2ed440c72c55b4b821644
tabsoff <tabstabs90@gmail.com>
2022-09-23 12:16:18 +0300

    hui

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+alert("Hello");
+
 function calculate() {
 
-	const A4_1 = [
-        [1, 29],
-        [6, 24],
-        [11, 21],
-        [21, 18],
-        [51, 17],
-        [101, 16],
-        [501, 14]
+	// Кол-во, А4, А3
+	const priceBW = [
+        [1, 29, 36],
+        [6, 24, 29],
+        [11, 21, 26],
+        [21, 18, 23],
+        [51, 17, 22],
+        [101, 16, 21],
+        [501, 14, 19]
 	];
 
 	var result;
@@ -15,13 +18,14 @@ function calculate() {
 	var price;
 
 	for (var i = 6; i >= 0; i--) {
-	   if (sheets >= A4_1[i][0]) {
-	   		price = A4_1[i][1];
+	   if (sheets >= priceBW[i][0]) {
+	   		price = priceBW[i][1];
 	   		break;
-	   }
+	   }=
 	}
 
 	result = sheets * price;
 
 	document.getElementById("result").innerHTML = result;
+
 }
