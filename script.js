33d283b4b58343daf84c6e3f75e5162fa4ad994e
tabsoff <tabstabs90@gmail.com>
2022-09-28 12:23:13 +0300

    Add autoupdate, price for one sheet

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+ // [ЧБ/цвет][кол-во][А4][А3]
+const priceSheet = [
+	[[1, 29, 36],
+    [6, 24, 29],
+    [11, 21, 26],
+    [21, 18, 23],
+    [51, 17, 22],
+    [101, 16, 21],
+    [501, 14, 19]],
+
+    [[1, 69, 84],
+    [6, 67, 75],
+    [11, 63, 68],
+    [21, 60, 65],
+    [51, 45, 50],
+    [101, 40, 45],
+    [200, 35, 40]]
+];
+
+var result;
+var sheets;
+var price;
+var radio;
+
 function calculate() {
 
- // [ЧБ/цвет][кол-во][А4][А3]
-	const priceSheet = [
-		[[1, 29, 36],
-        [6, 24, 29],
-        [11, 21, 26],
-        [21, 18, 23],
-        [51, 17, 22],
-        [101, 16, 21],
-        [501, 14, 19]],
+	sheets = Number(document.getElementById("sheets").value);
 
-        [[1, 69, 84],
-        [6, 67, 75],
-        [11, 63, 68],
-        [21, 60, 65],
-        [51, 45, 50],
-        [101, 40, 45],
-        [200, 35, 40]]
-	];
-
-	var result;
-	var sheets = Number(document.getElementById("sheets").value);
-	var price;
-
- 	var radio = document.getElementsByName('color');
+ 	 radio = document.getElementsByName('color');
 	for (var i = 0; i < radio.length; i++) {
     	if (radio[i].checked) {
       		color = i;
@@ -48,6 +51,6 @@ function calculate() {
 
 	result = sheets * price;
 
-	document.getElementById("result").innerHTML = result;
+	document.getElementById("result").innerHTML = price + "₽ × " + sheets + " шт = " + result ;
 
 }
